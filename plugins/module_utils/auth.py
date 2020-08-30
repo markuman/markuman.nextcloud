d3b77fcd8a19db9fdcebca56b3bc17245ca2c037
Markus Bergholz <markuman@gmail.com>
2020-08-30 18:36:41 +0200

    add delete methode and change error handling for missing host, user or token

--- plugins/module_utils/auth.py
+++ plugins/module_utils/auth.py
@@ -5,15 +5,29 @@ from ansible.errors import AnsibleError
 class NextcloudHandler:
     def __init__(self, kwargs):
         self.HOST = kwargs.get('host') or os.environ.get('NEXTCLOUD_HOST')
-        self.USER = kwargs.get('user') or os.environ.get('NEXTCLOUD_USER')
-        self.TOKEN = kwargs.get('api_token') or os.environ.get('NEXTCLOUD_TOKEN')
+        if self.HOST is None:
+            raise AnsibleError('Unable to continue. No Nextcloud Host is given.')
 
-        if None in [self.HOST, self.USER, self.TOKEN]:
-            raise AnsibleError('Unable to perform nextcloud passwords lookup. '
-                                        'host, user, api_token and label are required.')
-    
+        self.USER = kwargs.get('user') or os.environ.get('NEXTCLOUD_USER')
+        if self.USER is None:
+            raise AnsibleError('Unable to continue. No Nextcloud User is given.')
+
+        self.TOKEN = kwargs.get('api_token') or os.environ.get('NEXTCLOUD_TOKEN')
+        if self.TOKEN is None:
+            raise AnsibleError('Unable to continue. No Nextcloud Token is given.')
+
     def get(self, path):
         r = requests.get(
+            'https://{HOST}/{PATH}'.format(HOST=self.HOST, PATH=path),
+            auth=(self.USER, self.TOKEN)
+        )
+        if r.status_code == 200:
+            return r
+        else:
+            raise AnsibleError('Nextcloud retured with status code {SC}'.format(r.status_code))
+
+    def delete(self, path):
+        r = requests.delete(
             'https://{HOST}/{PATH}'.format(HOST=self.HOST, PATH=path),
             auth=(self.USER, self.TOKEN)
         )
