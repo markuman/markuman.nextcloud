3d0052a3f70e013b2f2a884c23f0fa2dc5b1323c
Markus Bergholz <markuman@gmail.com>
2021-03-18 21:30:16 +0100

    last try

--- plugins/module_utils/auth.py
+++ plugins/module_utils/auth.py
@@ -40,6 +40,10 @@ class NextcloudHandler:
 
 
     def put(self, path, src):
+        import q
+        q('{HTTP}://{HOST}/{PATH}'.format(HTTP=self.HTTP, HOST=self.HOST, PATH=path))
+        q(self.USER)
+        q(self.TOKEN)
         r = requests.put(
             '{HTTP}://{HOST}/{PATH}'.format(HTTP=self.HTTP, HOST=self.HOST, PATH=path), 
             data=open(src, 'rb'), auth=(self.USER, self.TOKEN)
